efd0c572efdc1c2fdd6e41d381c5bfe2b14b637f
Camilia <mzalicam@gmail.com>
2019-10-25 12:57:02 -0400

    added new pages and styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'nav', loadChildren: './nav/nav.module#NavPageModule' },
   { path: 'foobar', loadChildren: './foobar/foobar.module#FoobarPageModule' },
+  { path: 'favorites', loadChildren: './favorites/favorites.module#FavoritesPageModule' },
+  { path: 'library', loadChildren: './library/library.module#LibraryPageModule' },
+  { path: 'preferences', loadChildren: './preferences/preferences.module#PreferencesPageModule' },
+  { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' },
 ];
 
 @NgModule({
